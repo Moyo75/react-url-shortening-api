f75e3f3c4c32b96d97c41fa8e5b7f657404f4198
John Ademoye <moyojohn188@gmail.com>
2021-03-15 18:27:13 +0000

    Copy button event

--- src/components/ShortLink.js
+++ src/components/ShortLink.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import '../utils/script';
+
 const ShortLink = () => {
   return (
     <div className={'link__container flex flex__column flex__jc-c'}>
       <p className={'initial__link'}>https://www.frontendmentor.io</p>
       <div className='link__seperator'></div>
       <p className={'shortened__link'}>https://reLink/k4lyk</p>
-      <button>Copy</button>
+      <button className={'copy__button'}>Copy</button>
     </div>
   );
 };
--- src/utils/script.js
+++ src/utils/script.js
@@ -1,8 +1,10 @@
 window.onload = function () {
   const menuButton = document.querySelector('.menu-button');
   const headerMenu = document.querySelector('.header__menu');
+  const copyButton = document.querySelector('.copy__button');
+  const shortenedLink = document.querySelector('.shortened__link');
 
-  menuButton.addEventListener('click', () => {
+  function handleToggleMenu() {
     if (!headerMenu.classList.contains('state')) {
       // As the menu becomes visible
       headerMenu.classList.add('fade-in');
@@ -14,5 +16,40 @@ window.onload = function () {
       headerMenu.classList.add('fade-out');
       headerMenu.classList.remove('state');
     }
-  });
+  }
+
+  function handleCopyButton(event) {
+    event.preventDefault();
+
+    const selection = window.getSelection();
+    const range = new Range();
+    console.log(range);
+    range.selectNodeContents(shortenedLink);
+
+    selection.removeAllRanges();
+    selection.addRange(range);
+
+    try {
+      document.execCommand('copy');
+      selection.removeAllRanges();
+
+      copyButton.style.backgroundColor = '#3B3054';
+      copyButton.textContent = 'Copied!';
+
+      setTimeout(() => {
+        copyButton.style.backgroundColor = '#2ACFCF';
+        copyButton.textContent = 'Copy';
+      }, 3000);
+    } catch (error) {
+      setTimeout(() => {
+        copyButton.style.backgroundColor = '#f46262';
+        copyButton.textContent = "Can't copy, hit Ctrl+C!";
+      }, 2000);
+    }
+
+    // console.log(shortenedLink);
+  }
+
+  menuButton.addEventListener('click', handleToggleMenu);
+  copyButton.addEventListener('click', handleCopyButton);
 };
