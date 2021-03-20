f868dd275b6cea039c41a46b7aae988158272eac
John Ademoye <moyojohn188@gmail.com>
2021-03-20 12:48:47 +0000

    Fixed the after elements in the features list

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,46 +7,52 @@ import iconInstagram from '../images/icon-instagram.svg';
 
 const Footer = () => {
   return (
-    <div className={'footer flex flex__column flex__jc-c flex__ai-c'}>
-      <h1 translate={'no'}>Shortly</h1>
+    <section className={'footer__section'}>
       <div
-        className={'footer__list-set flex flex__column flex__jc-c flex__ai-c'}
+        className={'container footer flex flex__column flex__jc-c flex__ai-c'}
       >
-        <h3>Features</h3>
-        <ul>
-          <li>Link Shortening</li>
-          <li>Branded Links</li>
-          <li>Analytics</li>
-        </ul>
+        <h1 translate={'no'}>Shortly</h1>
+        <div
+          className={
+            'footer__list-set  flex flex__column flex__jc-c flex__ai-c'
+          }
+        >
+          <h3>Features</h3>
+          <ul className={'first__list'}>
+            <li>Link Shortening</li>
+            <li>Branded Links</li>
+            <li>Analytics</li>
+          </ul>
+        </div>
+        <div
+          className={'footer__list-set flex flex__column flex__jc-c flex__ai-c'}
+        >
+          <h3>Resources</h3>
+          <ul className={'second__list'}>
+            <li>Blog</li>
+            <li>Support</li>
+            <li>Developers</li>
+          </ul>
+        </div>
+        <div
+          className={'footer__list-set flex flex__column flex__jc-c flex__ai-c'}
+        >
+          <h3>Company</h3>
+          <ul className={'third__list'}>
+            <li>About</li>
+            <li>Our Team</li>
+            <li>Careers</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div className={'icon__list flex flex__ai-c flex__jc-se'}>
+          <img src={iconFacebook} alt={'Facebook Icon'} />
+          <img src={iconTwitter} alt={'Twitter Icon'} />
+          <img src={iconPinterest} alt={'Pinterest Icon'} />
+          <img src={iconInstagram} alt={'Instagram Icon'} />
+        </div>
       </div>
-      <div
-        className={'footer__list-set flex flex__column flex__jc-c flex__ai-c'}
-      >
-        <h3>Resources</h3>
-        <ul>
-          <li>Blog</li>
-          <li>Support</li>
-          <li>Developers</li>
-        </ul>
-      </div>
-      <div
-        className={'footer__list-set flex flex__column flex__jc-c flex__ai-c'}
-      >
-        <h3>Company</h3>
-        <ul>
-          <li>About</li>
-          <li>Our Team</li>
-          <li>Careers</li>
-          <li>Contact</li>
-        </ul>
-      </div>
-      <div className={'icon__list flex flex__ai-c flex__jc-se'}>
-        <img src={iconFacebook} alt='Facebook Icon' />
-        <img src={iconTwitter} alt='Twitter Icon' />
-        <img src={iconPinterest} alt='Pinterest Icon' />
-        <img src={iconInstagram} alt='Instagram Icon' />
-      </div>
-    </div>
+    </section>
   );
 };
 
